54fa0bf47832a8b075013d26b9d2f5fed5674509
Allie Kim <allie0147@gmail.com>
2021-01-04 15:47:22 +0900

    Initial Commit

---
+++ PageControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by ishift on 2021/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
